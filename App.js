cfe2c0c6182857b2294aab361117102894637bf8
namezio <canyouloveme1999@gmail.com>
2022-03-29 15:05:31 +0700

    update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import Notification from './NotificationScreen';
@@ -11,24 +11,20 @@ import ManagerRoomScreen from './siteScreens/ManagerRoomScreen';
 import signInScreen from './screen/SignInScreen';
 import ForgetPasswordScreen from './screen/ForgetPasswordScreen';
 
-
-
-
 const Stack = createNativeStackNavigator();
-const options = { headerShown: false };
+const options = {headerShown: false};
 function MyStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={LoginScreen} options={options} />
       <Stack.Screen name="Home" component={HomeScreen} options={options} />
-      <Stack.Screen name='Noti'component={Notification} options={options}/>
-      <Stack.Screen name='MeetingRoom' component={MeetingRoomScreen} />
-      <Stack.Screen name='JoinRoom' component={JoinRoomScreen}/>
-      <Stack.Screen name='CreateRoom' component={CreateRoomScreen}/>
-      <Stack.Screen name='ManagerRoom' component={ManagerRoomScreen}/>
-      <Stack.Screen name='SignIn' component={signInScreen} options={options}/>
-      <Stack.Screen name='Forget' component={ForgetPasswordScreen} />
-      
+      <Stack.Screen name="Noti" component={Notification} options={options} />
+      <Stack.Screen name="MeetingRoom" component={MeetingRoomScreen} />
+      <Stack.Screen name="JoinRoom" component={JoinRoomScreen} />
+      <Stack.Screen name="CreateRoom" component={CreateRoomScreen} />
+      <Stack.Screen name="ManagerRoom" component={ManagerRoomScreen} />
+      <Stack.Screen name="SignIn" component={signInScreen} options={options} />
+      <Stack.Screen name="Forget" component={ForgetPasswordScreen} />
     </Stack.Navigator>
   );
 }
@@ -40,4 +36,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
